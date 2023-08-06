3ecca4f9437c74e21d15d44462425048d9a4ac57
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2023-08-06 21:39:28 +0300

    Nested loops

---
+++ The C# Arc/nested-loops/04.Sum_of_Two_Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04.Sum_of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                for (int j = a; j <= b; j++)
+                {
+                    count++;
+                    if (i + j == n)
+                    {
+                        Console.WriteLine($"Combination N:{count} ({i} + {j} = {n})");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{count} combinations - neither equals {n}");
+        }
+    }
+}
---
+++ The C# Arc/nested-loops/05.Travelling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05.Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            while (country != "End")
+            {
+                double budget = double.Parse(Console.ReadLine());
+                double saved = 0;
+                while(saved < budget)
+                {
+                    double deposit = double.Parse(Console.ReadLine());
+                    saved += deposit;
+                }
+                Console.WriteLine($"Going to {country}!");
+
+                country = Console.ReadLine();
+            }
+        }
+    }
+}
