d7db7fa7dfbf2a33980d66a7a1fb0e40fcecc12f
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2023-08-10 00:55:46 +0300

    Nested loops exercise

---
+++ The C# Arc/nested-loops-exercise/01.Number_Pyramid/01.Number_Pyramid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06.Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int n = 1;
+            for(int rows = 1; rows <= x; rows++)
+            {
+                for(int cols = 1; cols <= rows; cols++)
+                {
+                    if(n > x)
+                    {
+                        return;
+                    } else
+                    {
+                        Console.Write($"{n} ");
+                        n++;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
