a2f7137dc1680ea1c08bc3c6f781621b97741f81
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2023-08-11 22:48:00 +0300

    Exam Preparation

---
+++ The C# Arc/exam-preparation/02.Mountain_Run/02.Mountain_Run/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.Mountian_Run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordSeconds = double.Parse(Console.ReadLine());
+            double distanceMeters = double.Parse(Console.ReadLine());
+            double timePerMeter = double.Parse(Console.ReadLine());
+
+            double time1 = distanceMeters * timePerMeter;
+            double everyFifty = Math.Floor(distanceMeters / 50) * 30;
+            double final = time1 + everyFifty;
+
+            if (final >= recordSeconds)
+            {
+                Console.WriteLine($"No! He was {final - recordSeconds:F2} seconds slower.");
+            } else
+            {
+                Console.WriteLine($"Yes! The new record is {final:F2} seconds.");
+            }
+        }
+    }
+}
