b378dbf351bfb8cc9d2567bbc3ca8a3dc3caf236
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2023-08-12 14:20:35 +0300

    Exam

---
+++ The C# Arc/exam/04.Computer_Firm/04.Computer_Firm/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _04.Computer_Firm 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sumRating = 0.0;
+            double final = 0.0;
+
+            for(int i = 1; i <= n; i++)
+            {
+                int rating = int.Parse(Console.ReadLine());
+                int lastDigit = rating % 10;
+                int possibleSales = rating / 10;
+                switch(lastDigit)
+                {
+                    case 2:
+                        final += 0;
+                        break;
+                    case 3:
+                        final += possibleSales * 0.5;
+                        break;
+                    case 4:
+                        final += possibleSales * 0.7;
+                        break;
+                    case 5:
+                        final += possibleSales * 0.85;
+                        break;
+                    case 6:
+                        final += possibleSales;
+                        break;
+                }
+
+                sumRating += lastDigit;
+            }
+
+            Console.WriteLine($"{final:F2}");
+            Console.WriteLine($"{sumRating / n:F2}");
+        }
+    }
+}
---
+++ The C# Arc/exam/05.Everest/05.Everest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _05.Everest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int day = 1;
+            int meters = 5364;
+            int goal = 8848;
+
+            while(input != "END")
+            {
+
+                if(input == "Yes")
+                {
+                    day += 1;
+                }
+
+                int climbed = int.Parse(Console.ReadLine());
+
+                if (day > 5)
+                {
+                    break;
+                } else
+                {
+                    meters += climbed;
+                }
+
+                if(meters >= goal)
+                {
+                    Console.WriteLine($"Goal reached for {day} days!");
+                    return;
+                } else
+                {
+                    input = Console.ReadLine();
+                }
+
+            }
+
+            Console.WriteLine("Failed!");
+            Console.WriteLine(meters);
+        }
+    }
+}
