f558c225e90ce1b47e4da235cf71838fde5486c1
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2023-08-12 14:19:02 +0300

    Exam

---
+++ The C# Arc/exam/01.PC_Store/01.PC_Store/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.PC_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceCPU = double.Parse(Console.ReadLine());
+            double priceGPU = double.Parse(Console.ReadLine());
+            double priceRAM = double.Parse(Console.ReadLine());
+            int countRam = int.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+
+            double priceCPULV = priceCPU * 1.57;
+            double priceGPULV = priceGPU * 1.57;
+            double priceRAMLV = priceRAM * 1.57;
+
+            double RAM = priceRAMLV * countRam;
+            double CPU = priceCPULV * (1 - discount);
+            double GPU = priceGPULV * (1 - discount);
+
+            Console.WriteLine($"Money needed - {RAM + CPU + GPU:F2} leva.");
+        }
+    }
+}
---
+++ The C# Arc/exam/03.Excursion_Calculator/03.Excursion_Calculator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _03.Excursion_Calculator 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            double priceOne = 0.0;
+
+            switch(season)
+            {
+                case "spring":
+                    if(people <= 5)
+                    {
+                        priceOne = 50;
+                    } else
+                    {
+                        priceOne = 48;
+                    }
+                    break;
+                case "summer":
+                    if (people <= 5)
+                    {
+                        priceOne = 48.5;
+                    }
+                    else
+                    {
+                        priceOne = 45;
+                    }
+                    break;
+                case "autumn":
+                    if (people <= 5)
+                    {
+                        priceOne = 60;
+                    }
+                    else
+                    {
+                        priceOne = 49.5;
+                    }
+                    break;
+                case "winter":
+                    if (people <= 5)
+                    {
+                        priceOne = 86;
+                    }
+                    else
+                    {
+                        priceOne = 85;
+                    }
+                    break;
+            }
+
+            double final = people * priceOne; 
+
+            if(season == "summer")
+            {
+                final *= 0.85;
+            } else if(season == "winter")
+            {
+                final *= 1.08;
+            }
+
+            Console.WriteLine($"{final:F2} leva.");
+        }
+    }
+}
