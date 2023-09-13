04f769ea23bb5a5dc46092ad7ec62916e7cfc310
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2023-09-13 18:30:30 +0300

    Conditional Statements

---
+++ conditional-statements-exercise/02.Bonus_Score/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace hustling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int points = Convert.ToInt32(Console.ReadLine());
+            double bonusPoints = 0;
+
+            if (points >= 0 && points <= 100)
+            {
+                bonusPoints += 5;
+            } else if (points > 100 && points <= 1000)
+            {
+                bonusPoints += points * 0.2;
+            } else
+            {
+                bonusPoints += points * 0.1;
+            }
+
+            if (points % 2 == 0)
+            {
+                bonusPoints += 1;
+            } else if (points % 5 == 0)
+            {
+                bonusPoints += 2;
+            }
+
+            Console.WriteLine(bonusPoints);
+            Console.WriteLine(points + bonusPoints);
+        }
+    }
+}
