e28f5b801e132ca740e62a7b7718308ef8ac6cf1
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2023-09-13 18:41:01 +0300

    First steps in coding exercise

---
+++ first-steps-in-coding-exercise/04.Vacation_Books_List/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace hustling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pagesCount = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int daysToRead = int.Parse(Console.ReadLine());
+
+            Console.Write(pagesCount / pagesPerHour / daysToRead);
+
+        }
+    }
+}
