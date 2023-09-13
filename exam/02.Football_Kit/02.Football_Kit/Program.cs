4ab0e6b2d07a8a2a89b938b0ad5aef19e0ebe47a
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2023-09-13 18:32:14 +0300

    Exam

---
+++ exam/02.Football_Kit/02.Football_Kit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02.Football_Kit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double shirt = double.Parse(Console.ReadLine());
+            double priceToReach = double.Parse(Console.ReadLine());
+
+            double shorts = shirt * 0.75;
+            double socks = shorts * 0.2;
+            double shoes = (shirt + shorts) * 2;
+            double sum = shirt + shorts + socks + shoes;
+
+            double afterDisc = sum * 0.85;
+
+            if(afterDisc >= priceToReach)
+            {
+                Console.WriteLine("Yes, he will earn the world-cup replica ball!");
+                Console.WriteLine($"His sum is {afterDisc:F2} lv.");
+            } else
+            {
+                Console.WriteLine("No, he will not earn the world-cup replica ball.");
+                Console.WriteLine($"He needs {priceToReach - afterDisc:F2} lv. more.");
+            }
+        }
+    }
+}
