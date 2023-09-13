3d0b3dd5977df10ba038e4da9b4928b99f23cb04
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2023-09-13 18:49:09 +0300

    Nested loops exercise

---
+++ nested-loops-exercise/04.Train_The_Trainers/04.Train_The_Trainers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04.Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            double final = 0.0;
+            int count = 0;
+            string presentationName = Console.ReadLine();
+
+            while (presentationName != "Finish")
+            {
+                int judges = int.Parse(input);
+                double sum = 0.0;
+
+                for(int i = 1; i <= judges; i++)
+                {
+                    sum += double.Parse(Console.ReadLine());
+                }
+
+                final += sum / judges;
+                count++;
+                Console.WriteLine($"{presentationName} - {sum / judges:F2}.");
+
+                presentationName = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Student's final assessment is {final / count:F2}.");
+        }
+    }
+}
